dda2aa624d3aa13f89bdd72d8a58b33272fcb94e
JaybhatiaPrimo <jayant.bhatia@primotech.com>
2026-05-05 16:24:45 +0530

    frontend compelteed

--- backend/agent.py
+++ backend/agent.py
@@ -5,7 +5,7 @@ from typing import TypedDict, List, Literal
 from langchain_tavily import TavilySearch
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_core.tools import tool 
-from langchain_core.messages import BaseMessage, HumanMessag,AIMessage
+from langchain_core.messages import BaseMessage, HumanMessage,AIMessage
 from pydantic import BaseModel,Field
 from langgraph.graph import StateGraph,END
 from langchain_groq import ChatGroq
